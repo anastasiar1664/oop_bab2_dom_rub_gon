434c0ba57c3a7fba4cb12cb93189fcb8b66e167e
TishkaaPlay <tix.tot@yandex.ru>
2026-05-06 14:09:38 +0300

    переход

---
+++ dynamicElements.js
@@ -0,0 +1,8 @@
+const addUserBtn = document.getElementById("addUserBtn");
+const usersList = document.getElementById("usersList");
+
+let userCounter = 1;
+
+addUserBtn.addEventListener("click", () => {
+  const listItem = document.createElement("li");
+});
